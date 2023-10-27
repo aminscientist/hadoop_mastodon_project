b2b8e9380ceb78d7b1665e0e34025cadebf0e6bf
aminscientist <136349041+aminscientist@users.noreply.github.com>
2023-10-27 09:07:34 +0100

    Update MapReduce.py

--- MapReduce.py
+++ MapReduce.py
@@ -6,13 +6,13 @@ from mrjob.step import MRStep
 class MapReduce(MRJob):
     def mapper(self,key,value):
         data = json.loads(value)
-        for x in data:
-            # username = x.get('account').get('username')
-            # followers = x.get('account').get('followers_count')
-            language = x.get("language")
+        for i in data:
+            username = i.get('account').get('username')
+            followers = i.get('account').get('followers_count')
+            language = i.get("language")
             yield(f"language:{language}",1)
-            # yield(f"followers:{username}",followers)
-            # yield(username, 1)
+            yield(f"followers:{username}",followers)
+            yield(username, 1)
 
     def reducer(self,key,values):
         yield(key, sum(values))
